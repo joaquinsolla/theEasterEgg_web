95c5ea243b38833f45b6d079974ece353f7e5023
Joaquín Solla Vázquez <joaquin.solla@udc.es>
2025-07-23 13:06:09 +0200

    Responsive design - Header

--- frontend/src/modules/app/components/Header.js
+++ frontend/src/modules/app/components/Header.js
@@ -11,39 +11,76 @@ const Header = () => {
     const user = useSelector(users.selectors.getUser);
 
     return (
-        <div className="Header">
-            <div className="Header-Banner-Container">
-                <Link to="">
-                    <img src="/theeasteregg_web/assets/header-logo-3.webp" className="Header-Banner" alt="logo"/>
-                </Link>
+        <div>
+            <div className="Header">
+                <div className="Header-Banner-Container">
+                    <Link to="">
+                        <img src="/theeasteregg_web/assets/header-logo-3.webp" className="Header-Banner" alt="logo"/>
+                    </Link>
+                </div>
+                <div>
+                    {isLoggedIn ? (
+                        <div className="Flex-center-div Header-Buttons-Container">
+                            <Link to="/for-you" className="Link-simple">
+                                <p>Para ti</p>
+                            </Link>
+                            <Link to="/desired-games" className="Link-simple Flex-center-div Margin-left Flex-center-div">
+                                <p>Lista de deseados</p>
+                            </Link>
+                            <Link to="/account" className="Link-simple Flex-center-div Margin-left Flex-center-div">
+                                <p>{user.userName}</p>
+                            </Link>
+                            <Link to="/logout" className="Link-simple Flex-center-div Margin-left Flex-center-div Logout-Container">
+                                <p>Cerrar sesión</p>
+                                <TbLogout className="Header-User-Icon Margin-left-small"/>
+                            </Link>
+                        </div>
+                    ) : (
+                        <div className="Flex-center-div Header-Buttons-Container">
+                            <Link to="/signup" className="Link-simple">
+                                <p>Crear cuenta</p>
+                            </Link>
+                            <Link to="/login" className="Link-simple Margin-left">
+                                <p>Iniciar sesión</p>
+                            </Link>
+                        </div>
+                    )}
+                </div>
             </div>
-            <div>
-                {isLoggedIn ? (
-                    <div className="Flex-center-div Header-Buttons-Container">
-                        <Link to="/for-you" className="Link-simple">
-                            <p>Para ti</p>
-                        </Link>
-                        <Link to="/desired-games" className="Link-simple Flex-center-div Margin-left Flex-center-div">
-                            <p>Lista de deseados</p>
-                        </Link>
-                        <Link to="/account" className="Link-simple Flex-center-div Margin-left Flex-center-div">
-                            <p>{user.userName}</p>
-                        </Link>
-                        <Link to="/logout" className="Link-simple Flex-center-div Margin-left Flex-center-div Logout-Container">
-                            <p>Cerrar sesión</p>
-                            <TbLogout className="Header-User-Icon Margin-left-small"/>
-                        </Link>
-                    </div>
-                ) : (
-                    <div className="Flex-center-div Header-Buttons-Container">
-                        <Link to="/signup" className="Link-simple">
-                            <p>Crear cuenta</p>
-                        </Link>
-                        <Link to="/login" className="Link-simple Margin-left">
-                            <p>Iniciar sesión</p>
-                        </Link>
-                    </div>
-                )}
+            <div className="Header HeaderSpacer Not-displayed">
+                <div className="Header-Banner-Container">
+                    <Link to="">
+                        <img src="/theeasteregg_web/assets/header-logo-3.webp" className="Header-Banner" alt="logo"/>
+                    </Link>
+                </div>
+                <div>
+                    {isLoggedIn ? (
+                        <div className="Flex-center-div Header-Buttons-Container">
+                            <Link to="/for-you" className="Link-simple">
+                                <p>Para ti</p>
+                            </Link>
+                            <Link to="/desired-games" className="Link-simple Flex-center-div Margin-left Flex-center-div">
+                                <p>Lista de deseados</p>
+                            </Link>
+                            <Link to="/account" className="Link-simple Flex-center-div Margin-left Flex-center-div">
+                                <p>{user.userName}</p>
+                            </Link>
+                            <Link to="/logout" className="Link-simple Flex-center-div Margin-left Flex-center-div Logout-Container">
+                                <p>Cerrar sesión</p>
+                                <TbLogout className="Header-User-Icon Margin-left-small"/>
+                            </Link>
+                        </div>
+                    ) : (
+                        <div className="Flex-center-div Header-Buttons-Container">
+                            <Link to="/signup" className="Link-simple">
+                                <p>Crear cuenta</p>
+                            </Link>
+                            <Link to="/login" className="Link-simple Margin-left">
+                                <p>Iniciar sesión</p>
+                            </Link>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
