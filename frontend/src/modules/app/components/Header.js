8fd7ca135431943739346b91465f0e2d84fe9ce4
Joaquín Solla Vázquez <joaquin.solla@udc.es>
2025-07-23 11:35:10 +0200

    Responsive design - Header

--- frontend/src/modules/app/components/Header.js
+++ frontend/src/modules/app/components/Header.js
@@ -17,27 +17,25 @@ const Header = () => {
                     <img src="/theeasteregg_web/assets/header-logo-3.webp" className="Header-Banner" alt="logo"/>
                 </Link>
             </div>
-            <div className="Header-Buttons-Container">
+            <div>
                 {isLoggedIn ? (
-                    <div className="Flex-center-div">
-                        <div className="Flex-center-div Header-Buttons-Container-Userbuttons">
-                            <Link to="/for-you" className="Link-simple">
-                                <p>Para ti</p>
-                            </Link>
-                            <Link to="/desired-games" className="Link-simple Flex-center-div Margin-left Flex-center-div">
-                                <p>Lista de deseados</p>
-                            </Link>
-                            <Link to="/account" className="Link-simple Flex-center-div Margin-left Flex-center-div">
-                                <p>{user.userName}</p>
-                            </Link>
-                        </div>
+                    <div className="Flex-center-div Header-Buttons-Container">
+                        <Link to="/for-you" className="Link-simple">
+                            <p>Para ti</p>
+                        </Link>
+                        <Link to="/desired-games" className="Link-simple Flex-center-div Margin-left Flex-center-div">
+                            <p>Lista de deseados</p>
+                        </Link>
+                        <Link to="/account" className="Link-simple Flex-center-div Margin-left Flex-center-div">
+                            <p>{user.userName}</p>
+                        </Link>
                         <Link to="/logout" className="Link-simple Flex-center-div Margin-left Flex-center-div Logout-Container">
                             <p>Cerrar sesión</p>
                             <TbLogout className="Header-User-Icon Margin-left-small"/>
                         </Link>
                     </div>
                 ) : (
-                    <div className="Flex-center-div">
+                    <div className="Flex-center-div Header-Buttons-Container">
                         <Link to="/signup" className="Link-simple">
                             <p>Crear cuenta</p>
                         </Link>
